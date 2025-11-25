290432eeec1f647708f5403e2f09433af56c6cdb
Divasdev <sharmadivas881@gmail.com>
2025-11-25 17:03:39 +0530

    files changed

--- Weather App/scripts.js
+++ Weather App/scripts.js
@@ -9,13 +9,13 @@ const cityName = document.querySelector('.city');
 const humidity = document.querySelector('.humidity');
 const windSpeed = document.querySelector('.wind');
 
-const pressureUnit=document.querySelector('.press');
-const apiKey="b4211a78c0cc6323e0287901ff614c4c";
-const apiUrl="https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+const pressureUnit = document.querySelector('.press');
+const apiKey = "b4211a78c0cc6323e0287901ff614c4c";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
 
 
-async function checkWeather(city){
+async function checkWeather(city) {
    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
    if (!response.ok) {
       alert("Invalid city Name");
@@ -27,32 +27,32 @@ async function checkWeather(city){
    temp.innerHTML = Math.round(data.main.temp) + "°C";
    humidity.innerHTML = data.main.humidity + "%";
    windSpeed.innerHTML = data.wind.speed + "km/h";
-   weatherDesc.innerHTML=data.weather[0].description;
-   pressureUnit.innerHTML=data.main.pressure +  "mm/hg";
+   weatherDesc.innerHTML = data.weather[0].description;
+   pressureUnit.innerHTML = data.main.pressure + "mm/hg";
    // Use the 'main' field from the weather array and compare lowercase strings
    const weatherMain = data.weather[0].main.toLowerCase();
 
-   if (weatherMain === "clouds"){
-      weatherIcon.src = "./images/cloudy.png";
+   if (weatherMain === "clouds") {
+      weatherIcon.src = "cloudy.png";
    }
-   else if (weatherMain === "clear"){
-      weatherIcon.src = "./images/sun.png";
+   else if (weatherMain === "clear") {
+      weatherIcon.src = "sun.png";
    }
-   else if (weatherMain === "rain" || weatherMain === "drizzle"){
-      weatherIcon.src = "./images/heavy-rain.png";
+   else if (weatherMain === "rain" || weatherMain === "drizzle") {
+      weatherIcon.src = "heavy-rain.png";
    }
-   else if (weatherMain === "snow"){
-      weatherIcon.src = "./images/snow.png";
+   else if (weatherMain === "snow") {
+      weatherIcon.src = "snow.png";
    }
-   else if (weatherMain === "mist"){
-      weatherIcon.src = "./images/humidity-icon.png";
+   else if (weatherMain === "mist") {
+      weatherIcon.src = "humidity-icon.png";
    }
-   else if (weatherMain== "haze"){
-      weatherIcon.src="./images/haze.png";
+   else if (weatherMain == "haze") {
+      weatherIcon.src = "haze.png";
    }
    else {
       // fallback icon if none of the above match
-      weatherIcon.src = "./images/default.png";
+      weatherIcon.src = "default.png";
    }
 
    console.log(data);
@@ -65,8 +65,8 @@ searchBtn.addEventListener('click', (event) => {
    if (city) checkWeather(city);
 });
 
-searchBox.addEventListener('keydown',(event)=>{
-   if(event.key==='Enter'){
+searchBox.addEventListener('keydown', (event) => {
+   if (event.key === 'Enter') {
       checkWeather(searchBox.value);
    }
 });
