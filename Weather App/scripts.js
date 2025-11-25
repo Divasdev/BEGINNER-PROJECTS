b3396e22f00e1ede7d7faf14ef9b4047b004cfdb
Divasdev <sharmadivas881@gmail.com>
2025-11-25 14:09:14 +0530

    console.log(weatherIcon);

---
+++ Weather App/scripts.js
@@ -0,0 +1,16 @@
+//Selecting the DOM elements
+
+const searchBox=document.querySelector('.search-box input');
+const searchBtn=document.querySelector('.search-box button')
+const weatherIcon=document.getElementById('weather-icon');
+console.log(weatherIcon);
+const temp=document.querySelector('.temp');
+console.log(temp);
+const weatherDesc=document.querySelector('.type')
+console.log(weatherDesc);
+const cityName=document.querySelector('.city');
+console.log(cityName);
+const humidity=document.querySelector('.humidity');
+console.log(humidity);
+const windSpeed=document.querySelector('.wind');
+console.log(windSpeed);
