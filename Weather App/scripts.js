32b0cd6468eb1ddf8903c65fb17028b2db144583
Divasdev <sharmadivas881@gmail.com>
2025-11-25 15:45:34 +0530

    calling the openweather api

--- Weather App/scripts.js
+++ Weather App/scripts.js
@@ -1,16 +1,39 @@
 //Selecting the DOM elements
 
-const searchBox=document.querySelector('.search-box input');
+const searchBox=document.querySelector('.search-box input').value;
 const searchBtn=document.querySelector('.search-box button')
 const weatherIcon=document.getElementById('weather-icon');
-console.log(weatherIcon);
+
 const temp=document.querySelector('.temp');
-console.log(temp);
+
 const weatherDesc=document.querySelector('.type')
-console.log(weatherDesc);
+
 const cityName=document.querySelector('.city');
-console.log(cityName);
+
 const humidity=document.querySelector('.humidity');
-console.log(humidity);
+
 const windSpeed=document.querySelector('.wind');
-console.log(windSpeed);
+
+
+const apiKey="b4211a78c0cc6323e0287901ff614c4c";
+const apiUrl="https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+async function checkWeather(city){
+
+   const response =await fetch(apiUrl +city +`&appid=${apiKey}`);
+   if (response.status==404){
+      alert("Invalid city Name");
+   }
+   var data = await response.json();
+   console.log(data.main);
+   console.log(data.weather);
+   console.log(data.wind);
+
+}
+
+  const events =searchBtn.addEventListener('click', ((event)=>{
+      const searchBox = document.querySelector('.search-box input').value;
+      
+      checkWeather(searchBox);
+ }));
+ 
